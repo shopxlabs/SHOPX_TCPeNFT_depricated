83807f97bbf023fdc548b456f230b63ff131e6ae
dipen <dhru_7777@yahoo.com>
2018-05-10 19:35:37 -0700

    updates to simplify code

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var SatToken = artifacts.require("./SatToken.sol");
+var SplytTracker = artifacts.require("./SplytTracker.sol");
 
 module.exports = function(deployer) {
+  var name = "SPLYT";
+  var desc = "Global inventory on the blockchain";
+  var ver  = 3;
+
   deployer.deploy(Migrations);
+
+  deployer.deploy(SatToken, name, desc, ver).then(async function() {
+    var deployed = await deployer.deploy(SplytTracker, ver, name, SatToken.address);
+  });
+
 };
