33110377fc4047eba996a5975e7f6a26bbf9ba8b
scott kim <scott@scotts-Mac-mini.local>
2018-07-26 19:33:20 -0700

    added modifer for total cost and status if buyer wants to order

--- contracts/OrderManager.sol
+++ contracts/OrderManager.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.24;
 
 import "./Order.sol";
 import "./OrderData.sol";
+import "./AssetBase.sol";
 
 contract OrderManager {
     
@@ -15,13 +16,23 @@ contract OrderManager {
         require(msg.sender == splytManagerAddress);
         _;
     }
-    
-    
+
+    modifier onlyAssetActive(address _assetAddress) {
+        require(AssetBase.AssetStatuses.ACTIVE == Asset(_assetAddress).assetStatus());
+        _;
+    }    
+    //@desc if buyer commits full amount of the price
+    modifier onlyPIF(address _assetAddress, uint _tokenAmount) {
+        Asset tmp = Asset(_assetAddress);
+        require(_tokenAmount == tmp.totalCost());
+        _;
+    } 
+
     constructor(address _splytManagerAddress) public {
        splytManagerAddress = _splytManagerAddress;
     }
 
-    function createOrder(address _assetAddress, address _buyer, uint _qty, uint _tokenAmount) public onlySplytManager {
+    function createOrder(address _assetAddress, address _buyer, uint _qty, uint _tokenAmount) public onlySplytManager onlyAssetActive(_assetAddress) {
         Order order = new Order(_assetAddress, _buyer, _qty, _tokenAmount);
         orderData.save(address(order));
     }
