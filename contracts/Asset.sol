8a2038caf38ef6f705016670db66e7b4a96d8d17
scott kim <scott@scotts-Mac-mini.local>
2018-08-13 15:40:31 -0700

    removed refactored code that was moved to the managers contracts

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -9,20 +9,20 @@ import "./Asset.sol";
 // a function and its input/output parameters. 
 // Use in conjuction to real contract's address, you can interact with external
 // contract's functions using this interface layer
-contract TrackerInterface {
+// contract TrackerInterface {
     
-    function internalContribute (address _from, address _to, uint _amount) 
-        public returns (bool);
+//     function internalContribute (address _from, address _to, uint _amount) 
+//         public returns (bool);
         
-    function internalRedeemFunds (
-        address _listingAddress, 
-        address _seller, 
-        uint _amount) public returns (bool);
+//     function internalRedeemFunds (
+//         address _listingAddress, 
+//         address _seller, 
+//         uint _amount) public returns (bool);
         
-    function getBalance(address _wallet) public returns (uint);
+//     function getBalance(address _wallet) public returns (uint);
     
-    function internalArbitrate(string _reason, address _requestedBy) public returns (address);
-}
+//     function internalArbitrate(string _reason, address _requestedBy) public returns (address);
+// }
 
 contract Asset is Events, Owned {
     
@@ -32,7 +32,6 @@ contract Asset is Events, Owned {
     enum Statuses { NOT_MINED, ACTIVE, IN_ARBITRATION, EXPIRED, SOLD_OUT, CLOSED, OTHER }
     Statuses public status;
     
-    TrackerInterface public tracker;
     address public seller;
     address[] listOfMarketPlaces;
     bytes12 public assetId;
@@ -47,19 +46,18 @@ contract Asset is Events, Owned {
     uint public initialStakeAmount;
 
     mapping(address => uint) contributions;
-    enum report{ SPAM, BROKEN, NOTRECIEVED, NOREASON  }
-    address arbitrateAddr = 0x0;
+    // address arbitrateAddr = 0x0;
     
-    Arbitration arbitration;
+    address arbitration;
     
     uint public inventoryCount;
 
     //User must have enough funds to call functions
-    modifier onlyHasEnoughFunds(address _reporter) {
-        uint balance = tracker.getBalance(_reporter);
-        require(balance >= initialStakeAmount);
-        _;
-    }
+    // modifier onlyHasEnoughFunds(address _reporter) {
+    //     uint balance = tracker.getBalance(_reporter);
+    //     require(balance >= initialStakeAmount);
+    //     _;
+    // }
     
     //only user gets to call certain functions
     modifier onlySeller(address _seller) {
@@ -92,7 +90,6 @@ contract Asset is Events, Owned {
             expirationDate = _expirationDate;
             kickbackAmount = _mpAmount;
             listOfMarketPlaces.push(_mpAddress);
-            tracker = TrackerInterface(msg.sender);
             initialStakeAmount = _stakeAmount;
             inventoryCount = _inventoryCount;
 
@@ -114,14 +111,14 @@ contract Asset is Events, Owned {
     // Checks to see if asset is open for contributions or not
     // Based on expired or not, and if incoming contribution will overflow the asset or not
     function isOpenForContribution(uint _contributing) public constant returns (bool) {
-        if(arbitrateAddr != 0x0)
+        if(arbitration != 0x0)
             return false;
         if (isExpired())
            return false;
         uint willGoOverBoard = _contributing + amountFunded;
         if (willGoOverBoard > totalCost)
             return false;
-        if (arbitrateAddr != 0x000)
+        if (arbitration != 0x000)
             return false;
         return true;
     }
@@ -144,64 +141,64 @@ contract Asset is Events, Owned {
     }
     
     // Handles multi/single contribuitons
-    function contribute(address _marketPlace, address _contributor, uint _contributing) public {
+    // function contribute(address _marketPlace, address _contributor, uint _contributing) public {
 
-        if (!isOpenForContribution(_contributing))
-            revert();
+    //     if (!isOpenForContribution(_contributing))
+    //         revert();
         
-        bool result;
-        uint userBalance = tracker.getBalance(_contributor);
-        if (userBalance < _contributing)
-            revert();
-        listOfMarketPlaces.push(_marketPlace);
+    //     bool result;
+    //     uint userBalance = tracker.getBalance(_contributor);
+    //     if (userBalance < _contributing)
+    //         revert();
+    //     listOfMarketPlaces.push(_marketPlace);
         
-        if (isFractional()) {
-            result = tracker.internalContribute(_contributor, this, _contributing);
-            if(result == true)
-                addToContributions(_contributor, _contributing);
-            releaseFunds();
+    //     if (isFractional()) {
+    //         result = tracker.internalContribute(_contributor, this, _contributing);
+    //         if(result == true)
+    //             addToContributions(_contributor, _contributing);
+    //         releaseFunds();
             
-        } else if (_contributing >= totalCost) {
-            uint mpGets;
-            uint sellerGets;
-            (mpGets, sellerGets) = calcDistribution();
-            tracker.internalContribute(_contributor, seller, sellerGets);
-            addToContributions(_contributor, _contributing);
-            if(mpGets > 0)
-                for(uint i = 0; i < listOfMarketPlaces.length; i++)
-                    tracker.internalContribute(_contributor, listOfMarketPlaces[i], mpGets);
-        } else
-            revert();
-    }
+    //     } else if (_contributing >= totalCost) {
+    //         uint mpGets;
+    //         uint sellerGets;
+    //         (mpGets, sellerGets) = calcDistribution();
+    //         tracker.internalContribute(_contributor, seller, sellerGets);
+    //         addToContributions(_contributor, _contributing);
+    //         if(mpGets > 0)
+    //             for(uint i = 0; i < listOfMarketPlaces.length; i++)
+    //                 tracker.internalContribute(_contributor, listOfMarketPlaces[i], mpGets);
+    //     } else
+    //         revert();
+    // }
     
     // Calculate how much seller gets after kickbacks taken out
-    function calcDistribution() public constant returns (uint, uint) {
+    // function calcDistribution() public constant returns (uint, uint) {
         
-        uint kickbackWitheld = kickbackAmount / listOfMarketPlaces.length;
-        uint sellerGets = totalCost - kickbackWitheld * listOfMarketPlaces.length;
-        return (kickbackWitheld, sellerGets);
-    }
+    //     uint kickbackWitheld = kickbackAmount / listOfMarketPlaces.length;
+    //     uint sellerGets = totalCost - kickbackWitheld * listOfMarketPlaces.length;
+    //     return (kickbackWitheld, sellerGets);
+    // }
     
-    // Updates this contract's metadata
-    function addToContributions(address _contributor, uint _contributing) private {
-        amountFunded += _contributing;
-        contributions[_contributor] += _contributing;
-    }
+    // // Updates this contract's metadata
+    // function addToContributions(address _contributor, uint _contributing) private {
+    //     amountFunded += _contributing;
+    //     contributions[_contributor] += _contributing;
+    // }
     
     // This is specifically for fractional sellers
     // It will release funds to seller if listing is fully funded and the listing is expired
-    function releaseFunds() public {
+    // function releaseFunds() public {
         
-        if (isFunded()) {
-            TrackerInterface trackerContract = TrackerInterface(tracker);
-            uint mpGets; 
-            uint sellerGets;
-            (mpGets, sellerGets) = calcDistribution();
-            trackerContract.internalRedeemFunds(this, seller, sellerGets);
-            for (uint i = 0; i < listOfMarketPlaces.length; i++)
-                trackerContract.internalRedeemFunds(this, listOfMarketPlaces[i], mpGets);
-        }
-    }
+    //     if (isFunded()) {
+    //         TrackerInterface trackerContract = TrackerInterface(tracker);
+    //         uint mpGets; 
+    //         uint sellerGets;
+    //         (mpGets, sellerGets) = calcDistribution();
+    //         trackerContract.internalRedeemFunds(this, seller, sellerGets);
+    //         for (uint i = 0; i < listOfMarketPlaces.length; i++)
+    //             trackerContract.internalRedeemFunds(this, listOfMarketPlaces[i], mpGets);
+    //     }
+    // }
  
     function addInventory(uint _qty) public onlyOwner {
         inventoryCount += _qty;
@@ -219,34 +216,5 @@ contract Asset is Events, Owned {
         inventoryCount = _count;
     }   
 
-    // report spam assets
-    // start refund process
-    /*
-    function arbitrate(Arbitration.Reasons _reason, address _reporter) public onlyHasEnoughFunds(_reporter) returns (bool) {
-        
-        // create arbitrate contract and append pause process to all functions.
-        arbitration = new Arbitration(this, _reason, _reporter);
-        arbitration.setReporterStake(initialStakeAmount);
-        
-        //reporter stakes the same amount so initial stake amount should be 2x now
-        tracker.internalContribute (_reporter, this, initialStakeAmount); 
-        emit Success(3, arbitrateAddr);
-        return true;        
-        
-    }
-    
-    // dispute reported spam
-    // only seller can call
-    function disputeArbitrationBySeller(address _seller) public onlySeller(_seller) onlyHasEnoughFunds(_seller) returns (bool) {
-        
-        arbitration.setDisputedStakeBySeller(initialStakeAmount);
-        
-        //now seller has 2x stake amoount
-        //original stake amount + disputed stake amoount = total stake amount by seller
-        tracker.internalContribute (_seller, this, initialStakeAmount); 
-        emit Success(4, arbitrateAddr);
-        return true;        
-        
-    }
-    */    
+     
 }
