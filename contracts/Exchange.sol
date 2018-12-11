0d7155a9a8132c8998520a9529bee223385581df
scott kim <scott@scotts-Mac-mini.local>
2018-12-11 12:45:16 -0800

    initial exchange rate contract

---
+++ contracts/Exchange.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+import './Owned.sol';  //change to interface later
+
+contract Exchange is Owned {
+        
+    uint public currentRate;
+
+    //@dev 4 assumed decimals
+    constructor(uint _exchangeRate) public {
+        currentRate = currentRate;
+    }  
+    
+    //@dev set rate per token
+    function setExchangeRate(uint _exchangeRate) public onlyOwner {
+        currentRate = _exchangeRate;
+    }    
+
+    //@dev convert wei to tokens
+    function convertWeiTokens(uint _wei) public view returns (uint) {
+        return (_wei * currentRate);
+    } 
+
+}
