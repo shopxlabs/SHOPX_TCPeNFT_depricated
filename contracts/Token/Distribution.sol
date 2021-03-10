aab9174d9b0bedfa4e172dad9cdecb7a247b2f2c
dipenjoshi <dhru_7777@yahoo.com>
2021-03-10 09:44:56 -0800

    final commit before v2.3 branch

---
+++ contracts/Token/Distribution.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import "../Utils/Owned.sol";
+import "../Utils/SafeMath.sol";
+
+contract LockVest is Owned {
+    
+    using SafeMath for uint256;
+
+    
+    
+
+}
