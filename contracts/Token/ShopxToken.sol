a125e74cab9b3f9c991bd5f8e846960de4df363e
dipenjoshi <dhru_7777@yahoo.com>
2021-03-24 16:38:19 -0700

    Distribution script updates Consistant compiler versions

--- contracts/Token/ShopxToken.sol
+++ contracts/Token/ShopxToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.4;
+pragma solidity 0.7.3;
 
 import "./ERC20.sol";
 import "../Utils/SafeMath.sol";
--- migrations/1_tge.js
+++ migrations/1_tge.js
@@ -1,4 +1,3 @@
-var Migrations = artifacts.require("./Migrations.sol")
 var ERC20 = artifacts.require("./Token/ERC20.sol")
 var Vesting = artifacts.require("./Token/Vesting.sol")
 
@@ -7,7 +6,7 @@ var Fs = require('fs')
 var Path = require('path')
 var CsvToJson = require('csvtojson')
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
 
   const name = "Splyt SHOPX tokens"
   const symbol = "SHOPX"
@@ -22,24 +21,68 @@ module.exports = function(deployer, network, accounts) {
 
   console.log(Chalk.green('Deploy wallet address: ' + walletConfig.from))
 
+  const aMonth = 2629743
   const logFilePath = InitLogFile()
-  // console.log('starting to load csv')
-  // var csvDistribution = await LoadCsv()
-  // console.log('loaded csv data')
-  deployer.deploy(Migrations, walletConfig)
 
-  deployer.deploy(ERC20, name, symbol, walletConfig)
-  .then(erc20 => {
-  
-    Log('ERC20 Deployed')
-    Log('TxHash: ' + erc20.transactionHash)
-    Log('ContractAddress: ' + erc20.address)
+  // Load csv file
+  const csvFilePath = Path.resolve('./[Mock] Token Distribution List - Sheet1.csv')
+  const csvData = await CsvToJson().fromFile(csvFilePath)
+  Log("CSV imported with " + csvData.length + " rows")
 
-    LoadCsv(csvDistribution => {
-      console.log(csvDistribution);
+  var erc20 = await deployer.deploy(ERC20, name, symbol, walletConfig)
+
+  Log('ERC20 Deployed')
+  Log('TxHash: ' + erc20.transactionHash)
+  Log('ContractAddress: ' + erc20.address)
+
+  // deploy vestings
+  for(var vestingData of csvData) {
+    Log("------------")
+    Log("Deploying Vesting: ")
+    Log("BeneficiaryAddress: " + vestingData.BeneficiaryAddress)
+    Log("StartDate: " + vestingData.StartDate)
+    Log("CliffDurationMonth: " + vestingData.CliffDurationMonth)
+    Log("DurationMonth: " + vestingData.DurationMonth)
+    Log("Revocable: " + vestingData.Revocable)
+    Log("FirstMonthBonusPercent: " + vestingData.FirstMonthBonusPercent)
+
+    // skip over non vested contracts
+    if(vestingData.UsesVesting == 'FALSE'){
+      Log("Vesting false skipping")
+      continue
+    }
+
+    // in seconds
+    var start = Math.round(new Date(vestingData.StartDate).getTime()/1000)
+    var cliffDuration = vestingData.CliffDurationMonth * aMonth
+    var duration = vestingData.DurationMonth * aMonth + cliffDuration
+
+    // deploy vesting contract
+    var vesting = await deployer.deploy(Vesting,
+      vestingData.BeneficiaryAddress,
+      start,
+      cliffDuration,
+      duration,
+      vestingData.Revocable,
+      '0x36E30BB8cD97e363F5e251983c8fFF1eC7A06f3a',
+      vestingData.FirstMonthBonusPercent,
+      walletConfig)
+
+    Log('Vesting Deployed')
+    Log('TxHash: ' + vesting.transactionHash)
+    Log('ContractAddress: ' + vesting.address)
+
+    //mint to vesting contract
+    await erc20._mint(vesting.address, vestingData.TotalTokens, walletConfig)
+    .then(async (tx) => {
+      Log('Minted to vesting contract ' + vesting.address +  ' Tokens ' + vestingData.TotalTokens)
+      await erc20.balanceOf(vesting.address, walletConfig)
+      .then(balance => {
+        Log('Now Vesting Contract: ' + vesting.address + ' has balance of ' + balance)
+      })
     })
 
-  })
+  }//end forloop
 
   function InitLogFile() {
 
@@ -47,26 +90,23 @@ module.exports = function(deployer, network, accounts) {
     if (!Fs.existsSync(folderPath))
       fs.mkdirSync(folderPath, {recursive: true})
     const now = new Date()
-    const filePath = folderPath + '/' + now.getUTCFullYear() + '-' + now.getUTCMonth() + '-' + now.getUTCDate() + '_' + network
-    Fs.appendFileSync(filePath, '\n\n' + now.getUTCHours() + ':' + now.getUTCMinutes() + '-------------------------\n');
+    const filePath = folderPath + '/' + now.getUTCFullYear() + ':' + now.getUTCMonth() + ':' + now.getUTCDate() + '_' + network
+    Fs.appendFileSync(filePath, '\n\n' + LogPrefix() + '-------------------------\n');
     return filePath
   }
 
   function Log(message) {
     if(message.includes("err")) {
-      console.log(Chalk.red(message))
+      console.log(Chalk.red(LogPrefix() + ' ' + message))
     } else {
-      console.log(Chalk.green(message))
+      console.log(Chalk.green(LogPrefix() + ' ' + message))
     }
-    Fs.appendFileSync(logFilePath, message + '\n');
+    Fs.appendFileSync(logFilePath, LogPrefix() + ' ' + message + '\n');
   }
 
-  function LoadCsv(cb) {
-    const filePath = Path.resolve('./[Mock] Token Distribution List - Sheet1.csv')
-    CsvToJson().fromFile(filePath)
-    .then(distributionJson => {
-      cb(distributionJson)
-    })
+  function LogPrefix() {
+    const now = new Date()
+    return now.getUTCHours() + ':' + now.getUTCMinutes() + ':' + now.getUTCSeconds()
   }
 
 }
